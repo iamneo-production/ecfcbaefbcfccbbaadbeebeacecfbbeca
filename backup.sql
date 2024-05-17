2c5e2f12b0d8ae0414c59a02dfc66bb0e6bf2350
neojarvis <techadmin@iamneo.ai>
2024-05-17 10:36:43 +0000

    Auto-save-commit-5/17/2024--4:06:42-PM

--- backup.sql
+++ backup.sql
@@ -1,4 +1,4 @@
--- MariaDB dump 10.19  Distrib 10.5.18-MariaDB, for debian-linux-gnu (x86_64)
+-- MariaDB dump 10.19  Distrib 10.5.21-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: appdb
 -- ------------------------------------------------------
@@ -32,4 +32,4 @@ USE `appdb`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-03 11:09:22
+-- Dump completed on 2024-05-17 10:36:43
